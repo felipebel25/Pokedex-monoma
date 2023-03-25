917c65224a9fdbe203e1b18bad2d94ead30fca94
Felipe Medina <felipe.medina@samgreencorp.com>
2023-03-25 11:30:58 -0500

    arreglo en la pantalla de Profile cuando se hace la redireccion si no tiene token

--- src/components/atoms/AppLayout/AppLayout.tsx
+++ src/components/atoms/AppLayout/AppLayout.tsx
@@ -1,6 +1,7 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useContext } from "react";
 import Head from "next/head"
 import { NavBarDashboard } from "@molecules";
+import { AuthContext } from "@/context";
 
 interface Props extends PropsWithChildren {
     title: string;
@@ -9,6 +10,7 @@ interface Props extends PropsWithChildren {
 }
 
 export const AppLayout = ({ children, title, pageDescription, imageFullUrl }: Props) => {
+    const { user } = useContext(AuthContext)
     return (
         <>
             <Head>
@@ -17,9 +19,11 @@ export const AppLayout = ({ children, title, pageDescription, imageFullUrl }: Pr
                 <meta name="og:title" content={title} />
                 <meta name="og:description" content={pageDescription} />
             </Head>
-            <nav>
-                <NavBarDashboard />
-            </nav>
+            {user?.name &&
+                <nav>
+                    <NavBarDashboard />
+                </nav>
+            }
             <main style={{ height: "100vh", overflow: "hidden" }}>
                 {children}
             </main>
